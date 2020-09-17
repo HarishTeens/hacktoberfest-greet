1ca48ff54d8b7bf502a1b7ac36e0cf1849bdf020
Harish <33366456+HarishTeens@users.noreply.github.com>
2020-09-17 09:02:05 +0000

    v1.0.2 release

--- index.js
+++ index.js
@@ -8,15 +8,14 @@ const { url } = require('inspector');
 
 
 const gf = new giphy.GiphyFetch('sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh')
-let gifURL;
  
-// fetch 10 gifs
+// fetch 20 gifs
 
 const getGifs=async ()=>{
-  const gifs=await gf.search("dogs", {limit: 1 })
-  gifs.data.forEach((each)=>{
-    gifURL=each.images.original.url;
-  })
+  const limit=20;
+  const gifs=await gf.search("dogs", {limit: limit })
+  const randomIndex=Math.floor(Math.floor(Math.random() * 6) + 1 );
+  return gifs[randomIndex].images.original.url;
 }
 
 const makeComment=async (github_token,url,number,body)=>{
@@ -28,33 +27,25 @@ const makeComment=async (github_token,url,number,body)=>{
   console.log(new_comment);
 }
 
-try {
-  
-  const payload = github.context.payload
-  const github_token = core.getInput('GITHUB_TOKEN');
+const run=async ()=>{
+  try {  
+    const payload = github.context.payload
+    const github_token = core.getInput('GITHUB_TOKEN');  
+    const context = github.context;
+    const senderLogin=payload.sender.login;
+    const url=context.payload.pull_request.comments_url.substr(22);
+    // const senderLogin="hari";    
+    const pull_request_number = context.payload.pull_request.number;
+    // const pull_request_number="1";
 
-  const context = github.context;
-  const senderLogin=payload.sender.login;
-  const url=context.payload.pull_request.comments_url.substr(22);
-  // const senderLogin="hari";
-  if (context.payload.pull_request == null) {
-      core.setFailed('No pull request found.');
-      return;
-  }
-  const pull_request_number = context.payload.pull_request.number;
-  // const pull_request_number="1";
- 
-  
-  
-  getGifs().then(()=>{    
+    const gifURL=await getGifs();
     const message= '![image]('+gifURL+') \n  Hello '+senderLogin+' , '+
-    'Thanks for your Commits, keep it rolling and be patient until a Reviewer merges it. '+ 
-    'Until then hope this doggy keeps you company 🚀  . ';
-    makeComment(github_token,url,pull_request_number,message);
-  });
-  
-} catch (error) {
-  core.setFailed(error.message);
+      'Thanks for your Commits, keep it rolling and be patient until a Reviewer merges it. '+ 
+      'Until then hope this doggy keeps you company 🚀  . ';
+      makeComment(github_token,url,pull_request_number,message);    
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-//Abel PR
+run();
